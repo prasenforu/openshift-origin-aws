7bb74f516857e4d116fc227bcec605bd33d3955c
prasenforu <prasenforu@hotmail.com>
2017-09-14 15:15:27 +0530

    Update reset-ip.sh

--- reset-ip.sh
+++ reset-ip.sh
@@ -5,8 +5,8 @@ PRI_IP=`(curl http://169.254.169.254/latest/meta-data/local-ipv4)`
 OLD_IP=`more /etc/origin/master/master-config.yaml | grep console | grep  publicURL | cut -d ":" -f3 | cut -d "/" -f3`
 sed -i "s/$OLD_IP/$PUB_IP/g" /etc/origin/master/master-config.yaml
 
-systemctl restart atomic-openshift-master
-systemctl status atomic-openshift-master
+kill -9 `ps -elf | grep open | grep -v grep | awk '{print $4}'`
+
 echo ""
 echo ""
 echo ""
