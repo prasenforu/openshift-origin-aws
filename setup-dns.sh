db0859b430a68fbb22b4f558dbd796576bb10fbe
prasenforu <prasenforu@hotmail.com>
2018-05-27 09:32:57 +0530

    Update setup-dns.sh

--- setup-dns.sh
+++ setup-dns.sh
@@ -49,7 +49,7 @@ sudo ssh-keygen -f /root/.ssh/id_rsa -N ''
 
 # Setting up yum repo for openshift
 
-#sudo cp /root/openshift-origin-aws/open.repo /etc/yum.repos.d/open.repo
+sudo cp /root/openshift-origin-aws/open.repo /etc/yum.repos.d/open.repo
 sudo yum clean all
 sudo yum repolist
 sudo yum -y update
