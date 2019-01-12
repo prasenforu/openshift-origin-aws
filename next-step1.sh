c038ac6b5a66ab23d847dfae5530439444259ef1
prasenforu <prasenforu@hotmail.com>
2019-01-12 17:13:14 +0530

    Update next-step1.sh

--- next-step1.sh
+++ next-step1.sh
@@ -5,10 +5,14 @@
 for node in {ose-master,ose-hub,ose-node1,ose-node2}; do
 echo "Deploy SSH Key on $node" && \
 scp -i prasen.pem /root/.ssh/id_rsa.pub centos@$node:/home/centos/.ssh/id_rsa.pub_root
+ssh centos@$node -i prasen.pem "sudo mkdir /root/.ssh"
+ssh centos@$node -i prasen.pem "sudo chmod 700 /root/.ssh"
+ssh centos@$node -i prasen.pem "sudo touch /root/.ssh/authorized_keys"
 ssh centos@$node -i prasen.pem "sudo mv /home/centos/.ssh/id_rsa.pub_root /root/.ssh/authorized_keys"
 ssh centos@$node -i prasen.pem "sudo chown root:root /root/.ssh/authorized_keys"
 ssh centos@$node -i prasen.pem "sudo chmod 600 /root/.ssh/authorized_keys"
 ssh centos@$node -i prasen.pem "sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config"
+ssh centos@$node -i prasen.pem "sudo sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config"
 ssh centos@$node -i prasen.pem "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
 ssh centos@$node -i prasen.pem "sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config"
 ssh centos@$node -i prasen.pem "sudo service sshd restart"
@@ -28,7 +32,8 @@ sleep 90
 
 for node in {ose-master,ose-hub,ose-node1,ose-node2}; do
 echo "Deploy Openshift Repo on $node" && \
-scp /root/openshift-origin-aws/open.repo $node:/etc/yum.repos.d/open.repo
+#scp /root/openshift-origin-aws/open.repo $node:/etc/yum.repos.d/open.repo
+scp /root/openshift-origin-aws/open311-gluster.repo $node:/etc/yum.repos.d/ocp311gluster.repo
 ssh $node "echo 'nameserver 8.8.8.8' | sudo tee --append /etc/resolv.conf"
 ssh $node "echo 'preserve_hostname: true' | sudo tee --append /etc/cloud/cloud.cfg"
 ssh $node "rm /etc/hostname"
