6fcb169a4290393a8bfafb50d2157d17e78979d3
prasenforu <prasenforu@hotmail.com>
2019-03-29 00:15:00 +0530

    Update scanner.sh

--- security/scanner.sh
+++ security/scanner.sh
@@ -2,6 +2,7 @@
 
 LOGPATH=/log/output.log
 DT=`date '+%d/%m/%Y %H:%M:%S'`
+MAILID="mailid@hotmail.com"
 
 alias scan='sh /usr/bin/scan.sh'
 
@@ -20,50 +21,111 @@ STAGE=`echo "$1" | jq  '.items [] .stage' | sed 's/"//g'`
 SUBRESOURCE=`echo "$1" | jq  '.items [] .objectRef.subresource' | sed 's/"//g'`
 REQUESTURI=`echo "$1" | jq  '.items [] .requestURI' | sed 's/"//g'`
 
-#### For POD/container login ########
+####### MAIL FOR AUTHENTICATION #######
 
-if [ "$ACTION" = "create" ] && [ "$CODE" = "101" ] && [ "$STAGE" = "ResponseStarted" ] && [ "$SUBRESOURCE" = "exec" ]; then
+mailsendauth () {
 
-     echo "[ $DT ]  User ($OCUSER) tried to login $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP)"
-     echo "[ $DT ]  User ($OCUSER) tried to login $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP)" >> $LOGPATH
-     exit
+if [ "$MAIL" = "y" ] || [ "$MAIL" = "Y" ]; then
+        sed "s/MSG/$MSG/g" /etc/webhook/mailtemplate.txt > /etc/webhook/mailbody.txt
+        /etc/webhook/mailsend.py "ALERT: Authentication concern from $SOURCEIP" "$MAILID"
+        exit
+else
+        exit
 fi
+}
+
+####### MAIL FOR RESOURCE #######
+
+mailsendrest () {
+
+if [ "$MAIL" = "y" ] || [ "$MAIL" = "Y" ]; then
+        sed "s/MSG/$MSG/g" /etc/webhook/mailtemplate.txt > /etc/webhook/mailbody.txt
+        /etc/webhook/mailsend.py "ALERT: Security issue on $RESOURCE ($OBJNAME)" "$MAILID"
+        exit
+else
+        exit
+fi
+}
+
+#### For POD/container ########
+
+if [ "$RESOURCE" = "pods" ] && [ "$ACTION" = "create" ] && [ "$CODE" = "101" ] && [ "$STAGE" = "ResponseStarted" ] && [ "$SUBRESOURCE" = "exec" ]; then
+
+     MSG="User ($OCUSER) tried to login $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP)"
+     echo "[ $DT ]  $MSG"
+     echo "[ $DT ]  $MSG" >> $LOGPATH
+     MAIL=y
+     mailsendrest
+fi
+
+if [ "$RESOURCE" = "pods" ] && [ "$ACTION" = "create" ] && [ "$CODE" = "201" ] && [ "$STAGE" = "ResponseComplete" ]; then
+
+     op=`scan -psv -ns $NS 2>&1 | grep -s "+--------" -A 150 | grep "\s$OBJNAME\s"`
+     op1=`scan -rp -ns $NS 2>&1 | grep -s "+--------" -A 150 | grep "\s$OBJNAME\s"`
+
+     if [ "$op1" != "" ]; then
+        MSG1="Its a risky POD"
+     fi
+
+     if [ "$op" != "" ]; then
+        MSG="POD ($OBJNAME) in project ($NS) service account (token) mounted. $MSG1."
+        echo "[ $DT ]  $MSG"
+        echo "[ $DT ]  $MSG" >> $LOGPATH
+        echo "$op" >> $LOGPATH
+        echo "$op1" >> $LOGPATH
+        MAIL=y
+        mailsendrest
+     else
+        exit
+     fi
+fi
+
 
 ##### for authentication #######
 
 if [ "$ACTION" = "post" ] && [ "$CODE" = "200" ]; then
 
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - Success"
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - Success" >> $LOGPATH
-   exit
+   MSG="Someone tried to login from this IP ($SOURCEIP) - Success"
+   echo "[ $DT ]  $MSG"
+   echo "[ $DT ]  $MSG" >> $LOGPATH
+   MAIL=y
+   mailsendauth
 fi
 
 if [ "$ACTION" = "get" ] && [ "$CODE" = "401" ] && [ "REASON" != "Unauthorized" ]; then
 
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $MESSAGE"
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $MESSAGE" >> $LOGPATH
-   exit
+   MSG="Someone tried to login from this IP ($SOURCEIP) - $MESSAGE"
+   echo "[ $DT ]  $MSG"
+   echo "[ $DT ]  $MSG" >> $LOGPATH
+   MAIL=y
+   mailsendauth
 fi
 
 if [ "$ACTION" = "head" ] && [ "$CODE" = "302" ]; then
 
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI"
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI" >> $LOGPATH
-   exit
+   MSG="Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI"
+   echo "[ $DT ]  $MSG"
+   echo "[ $DT ]  $MSG" >> $LOGPATH
+   MAIL=y
+   mailsendauth
 fi
 
 if [ "$ACTION" = "post" ] && [ "$CODE" = "302" ]; then
 
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI"
-   echo "[ $DT ]  Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI" >> $LOGPATH
-   exit
+   MSG="Someone tried to login from this IP ($SOURCEIP) - $REQUESTURI"
+   echo "[ $DT ]  $MSG"
+   echo "[ $DT ]  $MSG" >> $LOGPATH
+   MAIL=n
+   mailsendauth
 fi
 
 if [ "$ACTION" = "get" ] && [ "$CODE" = "200" ] && [ "$RESOURCE" = "users" ]; then
 
-   echo "[ $DT ]  User ($OCUSER) tried to login from this IP ($SOURCEIP) - $REQUESTURI"
-   echo "[ $DT ]  User ($OCUSER) tried to login from this IP ($SOURCEIP) - $REQUESTURI" >> $LOGPATH
-   exit
+   MSG="User ($OCUSER) tried to login from this IP ($SOURCEIP) - $REQUESTURI"
+   echo "[ $DT ]  $MSG"
+   echo "[ $DT ]  $MSG" >> $LOGPATH
+   MAIL=n
+   mailsendauth
 fi
 
 ##### for Resouces with create, delete, patch & bind  verb #######
@@ -76,61 +138,77 @@ if [ "$ACTION" = "create" ] || [ "$ACTION" = "delete" ] || [ "$ACTION" = "patch"
 
         if [ "$CODE" = "409" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
         elif [ "$CODE" = "404" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "200" ] || [ "$CODE" = "201" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success" >> $LOGPATH
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
 
             if [ "$RESOURCE" = "clusterrolebindings" ] && [ "$ACTION" = "create" ]; then
-               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 10 >> $LOGPATH
-               exit
+               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
+            fi
+            if [ "$RESOURCE" = "clusterrolebindings" ] && [ "$ACTION" = "patch" ]; then
+               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
             fi
             if [ "$RESOURCE" = "clusterroles" ] && [ "$ACTION" = "patch" ]; then
                op=`scan -aarbcr $OBJNAME 2>&1 | grep -v Associated | grep RoleBinding | cut -d "|" -f3`
-               scan -crru $op 2>&1 | grep -s "+--------" -A 10 >> $LOGPATH
-               exit
+               scan -crru $op 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
             fi
 
+            mailsendrest
         fi
 
     elif [ "$STATUS" != "Failure" ]; then
 
         if [ "$CODE" = "409" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "404" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "200" ] || [ "$CODE" = "201" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success" >> $LOGPATH
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
 
             if [ "$RESOURCE" = "clusterrolebindings" ] && [ "$ACTION" = "create" ]; then
-               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 10 >> $LOGPATH
-               exit
+               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
+            fi
+            if [ "$RESOURCE" = "clusterrolebindings" ] && [ "$ACTION" = "patch" ]; then
+               scan -crru $OBJNAME 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
             fi
             if [ "$RESOURCE" = "clusterroles" ] && [ "$ACTION" = "patch" ]; then
                op=`scan -aarbcr $OBJNAME 2>&1 | grep -v Associated | grep RoleBinding | cut -d "|" -f3`
-               scan -crru $op 2>&1 | grep -s "+--------" -A 10 >> $LOGPATH
-               exit
+               scan -crru $op 2>&1 | grep -s "+--------" -A 50 >> $LOGPATH
             fi
 
+            mailsendrest
         fi
     fi
  fi
@@ -147,46 +225,57 @@ if [ "$ACTION" = "create" ] || [ "$ACTION" = "delete" ] || [ "$ACTION" = "patch"
 
         if [ "$CODE" = "409" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
+
         elif [ "$CODE" = "404" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "200" ] || [ "$CODE" = "201" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success" >> $LOGPATH
-            exit
-
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
         fi
 
      elif [ "$STATUS" != "Failure" ]; then
 
         if [ "$CODE" = "409" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "404" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON" >> $LOGPATH
-            exit
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - $REASON"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
 
         elif [ "$CODE" = "200" ] || [ "$CODE" = "201" ]; then
 
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
-            echo "[ $DT ]  User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success" >> $LOGPATH
-            exit
-
+            MSG="User ($OCUSER) tried to $ACTION $RESOURCE ($OBJNAME) in project ($NS) from this IP ($SOURCEIP) - Success"
+            echo "[ $DT ]  $MSG"
+            echo "[ $DT ]  $MSG" >> $LOGPATH
+            MAIL=y
+            mailsendrest
         fi
      fi
   fi
 fi
 
-######################################################
+###############################
