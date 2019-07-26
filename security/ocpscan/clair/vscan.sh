a430a5bc26a16212ed9288b26dbe476f02d66a04
prasenforu <prasenforu@hotmail.com>
2019-07-26 13:18:06 +0530

    Update vscan.sh

--- security/ocpscan/clair/vscan.sh
+++ security/ocpscan/clair/vscan.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # This script will check Vulnerability of pod
 
-POD=yelb-ui-5946c9cfdb-wrpdb
-NS=sample-app
+RESOURCE=`echo "$1" | jq  '.eventmeta .kind'  | sed 's/"//g'`
+POD=`echo "$1" | jq  '.eventmeta .name'  | sed 's/"//g'`
+NS=`echo "$1" | jq  '.eventmeta .namespace'  | sed 's/"//g'`
+REASON=`echo "$1" | jq  '.eventmeta .reason'  | sed 's/"//g'`
+
 KUBEHOST=10.138.0.16
-
 TOKEN=`more $SERVICE_TOKEN_FILENAME`
-oc login https://$KUBEHOST:$KUBEPORT --token=$TOKEN --insecure-skip-tls-verify=true 1>/dev/null
 
-images_array=( `oc describe pod $POD -n $NS | grep Image: | awk "{print $2}"` )
+if [ "$RESOURCE" == "pod" ] && [ "$REASON" == "Created" ]; then
 
-for image in "${images_array[@]}"
-do
-   HIGH=`klar "$image" 2>/dev/null | grep  High: | cut -f2 -d " "`
+   oc login https://$KUBEHOST:$KUBEPORT --token=$TOKEN --insecure-skip-tls-verify=true 1>/dev/null
 
-   if [ $image != "" ] && [ $HIGH > 0 ]; then
-     echo "Image ($image) in POD ($POD) in NS ($NS) is a Vulnerable (High=$HIGH) image."
-   fi
-done
+   images_array=( `oc describe pod $POD -n $NS | grep Image: | awk "{print $2}"` )
+
+   for image in "${images_array[@]}"
+   do
+      HIGH=`klar "$image" 2>/dev/null | grep  High: | cut -f2 -d " "`
+      if [ $image != "" ] && [ $HIGH > 0 ]; then
+        echo "Image ($image) in POD ($POD) in NS ($NS) is a Vulnerable (High=$HIGH) image."
+      fi
+   done
+
+else
+  exit
+fi
