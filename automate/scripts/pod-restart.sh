a8df8e05098b81d193013c6f439e2103f2e8e72c
prasenforu <prasenforu@hotmail.com>
2019-09-15 07:28:12 +0530

    Create pod-restart.sh

---
+++ automate/scripts/pod-restart.sh
@@ -0,0 +1,32 @@
+    #!/bin/sh
+    ## POD restart script
+    
+    # OCP Authentication
+    
+    KUBETOKEN=`more /run/secrets/kubernetes.io/serviceaccount/token`
+    
+    if [ $KUBEPASS != "" ]; then
+       oc login https://$KUBEHOST:$KUBEPORT --username=$KUBEUSER --password=$KUBEPASS --insecure-skip-tls-verify=true
+    fi
+    
+    if [ $KUBETOKEN != "" ]; then
+       oc login https://$KUBEHOST:$KUBEPORT --token=$KUBETOKEN
+    fi
+    
+    # OCP project selection
+    oc project $1
+    
+    # Checking Status
+    echo "Application=$2 in namespace=$1 status before restart"
+    oc get po/$2
+    
+    # Deleting POD
+    # if [ $3 == "firing" ]; then
+       oc delete pod $2 --grace-period=0 
+    # fi
+    
+    # Checking Status & errors
+    sleep 10
+    echo "Application=$2 in namespace=$1 status after restart"
+    oc get po/$2
+    oc logs $2 -c promethus | grep bad
